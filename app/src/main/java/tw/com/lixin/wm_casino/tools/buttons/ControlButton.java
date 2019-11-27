e4d4bcd0f23a4d830d05b30f22e1c8ebfecd006b
hal0360 <atromoby@gmail.com>
2019-11-27 18:26:37 +0800

    stupid comiitdrf daad dadad aadd

--- app/src/main/java/tw/com/lixin/wm_casino/tools/buttons/ControlButton.java
+++ app/src/main/java/tw/com/lixin/wm_casino/tools/buttons/ControlButton.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import androidx.appcompat.widget.AppCompatTextView;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import tw.com.atromoby.widgets.CmdView;
@@ -16,7 +15,6 @@ public class ControlButton extends AppCompatTextView implements View.OnClickList
     private CmdView cmd;
     private Boolean disabled = true;
 
-
     public ControlButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray a = context.obtainStyledAttributes(attrs,R.styleable.ControlButton);
@@ -25,7 +23,7 @@ public class ControlButton extends AppCompatTextView implements View.OnClickList
         setBackgroundResource(R.drawable.controll_off);
         setGravity(Gravity.CENTER);
         setTextColor(0xffffffff);
-        setTextSize(TypedValue.COMPLEX_UNIT_DIP, 11);
+
         if(type == 1){
             setText(R.string.re_bet);
         }else if(type ==2){
@@ -44,10 +42,6 @@ public class ControlButton extends AppCompatTextView implements View.OnClickList
         }else{
             setBackgroundResource(R.drawable.control_on);
         }
-    }
-
-    public boolean isDisabled(){
-        return disabled;
     }
 
     public void clicked(CmdView cd){
